9cecc5ccd4c9986e61102b2c2faccc70edae24a3
Tallis Vanek <Tallis.Vanek@adtran.com>
2014-05-14 12:06:53 -0400

    automatically publishing and installing (PyroLibrary)

--- src/PyroLibrary/keywords/_browsermanagement.py
+++ src/PyroLibrary/keywords/_browsermanagement.py
@@ -197,7 +197,7 @@ class _BrowserManagementKeywords(KeywordGroup):
         """        
         print '(selenium_reload) suspend = %s' % suspend_after_element_found
         self._seleniumlib.reload_page()
-        if suspend_after_element_found != None:
+        if suspend_after_element_found not None:
             print '(selenium_reload) javascript suspendAll issued! (suspend_after_element_found == %s' % suspend_after_element_found
             self._seleniumlib.wait_until_element_is_visible(suspend_after_element_found, error='(selenium_reload) failed because the element was not found after reload')
             self._seleniumlib.execute_javascript('window.ADTRAN.store.RefreshBaseStore.suspendAll();')
